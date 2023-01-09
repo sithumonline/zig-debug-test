201267b681179e7530a159359e819aa4919b393b
MetaSky <creator_meta_sky@163.com>
2023-01-09 12:30:04 +0800

    try debug unit test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # zig-debug-test
+
+- [ ] 编译的过程怎么理解 ?
+- [ ] 显示 test debug 日志
---
+++ src/introducing_zig_test.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+fn main() void {
+    std.debug.print("123", .{});
+}
+
+test "expect addOne adds one to 41" {
+    std.debug.print("hello world", .{});
+
+    // The Standard Library contains useful functions to help create tests.
+    // `expect` is a function that verifies its argument is true.
+    // It will return an error if its argument is false to indicate a failure.
+    // `try` is used to return an error to the test runner to notify it that the test failed.
+
+    var value: i32 = addOne(41);
+
+    try std.testing.expect(value == 42);
+}
+
+/// The function `addOne` adds one to the number given as its argument.
+fn addOne(number: i32) i32 {
+    return number + 1;
+}
